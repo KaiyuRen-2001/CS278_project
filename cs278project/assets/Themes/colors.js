9f533d6e1eee782d5c2b12d945b0c10c4dcaaca3
Cole Lee <colelee@DN0a1f2cdc.SUNet>
2023-05-02 21:06:10 -0700

    design system

---
+++ cs278project/assets/Themes/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+    background: '#D9D9D9',
+    darkgrey: '#978CA6',
+    lightgrey: '#E4E6EB',
+    purple: '#8D70DC',
+    white: '#FFFFFF',
+    black: '#000000',
+};
---
+++ cs278project/assets/Themes/fonts.js
@@ -0,0 +1,7 @@
+export const fonts = {
+    family: 'poppins',
+    sectionHeaderSize: 30,
+    sectionHeaderStyle: "semi-bold",
+    bodySize: 30,
+    iconSize: 35,
+};
