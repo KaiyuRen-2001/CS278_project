9c323af9d7d4f38db6bd455072b1396fa4bf626f
KaiyuRen-2001 <85300992+KaiyuRen-2001@users.noreply.github.com>
2023-05-07 05:56:49 -0700

    1st push
    Finished main screen

--- cs278project/App.js
+++ cs278project/App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Text } from "react-native";
 
-export default function App() {
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from './screens/HomeScreen';
+
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
+
+export default function App() { 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ cs278project/screens/HomeScreen.js
@@ -0,0 +1,182 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  faHome,
+  faUser,
+  faPlus,
+  faEllipsis,
+} from "@fortawesome/free-solid-svg-icons";
+import { Image } from "react-native-elements";
+
+const ListItem = ({ imageSource, title, location, date, time }) => {
+  return (
+    <View style={styles.listItemContainer}>
+      <Image source={imageSource} style={styles.profileImage} />
+      <View style={styles.listItemTextContainer}>
+        <Text style={styles.listItemTitle}>{title}</Text>
+        <Text style={styles.listItemLocation}>{location}</Text>
+      </View>
+
+      <View style={styles.listItemText2Container}>
+        <Text style={styles.listItemDate}>{date}</Text>
+        <Text style={styles.listItemTime}>{time}</Text>
+      </View>
+      <TouchableOpacity style={styles.listItemEditButton}>
+        <FontAwesomeIcon icon={faEllipsis} size={20} color="#888" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={require("../assets/map.jpeg")} style={styles.image} />
+      </View>
+      <Text style={styles.heading}>Friends and Events</Text>
+      <ScrollView style={styles.scrollView}>
+        <ListItem
+          imageSource={require("../assets/stanford.png")}
+          title="Wilbur Dinner!"
+          location="Wilbur Dining"
+          date="May 1, 2023"
+          time="6:30pm"
+        />
+        <ListItem
+          imageSource={require("../assets/stanford.png")}
+          title="Cinco de Mayo Stern Dining"
+          location="Stern Dining"
+          date="May 5, 2023"
+          time="11:30am"
+        />
+        <ListItem
+          imageSource={require("../assets/stanford.png")}
+          title="Cinco de Mayo Party"
+          location="Mirrielees Dorm"
+          date="May 1, 2023"
+          time="10:30am"
+        />
+        <ListItem
+          imageSource={require("../assets/stanford.png")}
+          title="SVSA Field Day"
+          location="Wilbur Field"
+          date="May 6, 2023"
+          time="2:30pm"
+        />
+      </ScrollView>
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesomeIcon icon={faHome} size={30} color="#888" />
+          <Text style={styles.iconLabel}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesomeIcon icon={faPlus} size={30} color="#888" />
+          <Text style={styles.iconLabel}>Add Event</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.iconButton}>
+          <FontAwesomeIcon icon={faUser} size={30} color="#888" />
+          <Text style={styles.iconLabel}>Profile</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#4B0082",
+  },
+  imageContainer: {
+    alignItems: "center",
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  image: {
+    width: 380,
+    height: 380,
+  },
+  scrollView: {
+    paddingHorizontal: 20,
+  },
+  listItemContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderBottomWidth: 1,
+    borderBottomColor: "#fff",
+    paddingVertical: 10,
+  },
+  heading: {
+    fontWeight: "bold",
+    //flex: 1,
+    fontSize: 20,
+    color: "#fff",
+    marginLeft: 10,
+  },
+  listItemTextContainer: {
+    marginRight: 10,
+    textAlign: "left",
+  },
+  
+  profileImage: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    marginRight: 10,
+  },
+  listItemText: {
+    fontWeight: "bold",
+    //flex: 1,
+    color: "#fff",
+    marginRight: 10,
+  },
+  listItemTitle: {
+    color: "#fff",
+    marginRight: 10,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  listItemLocation: {
+    fontSize: 10,
+    color: "#F5DEB3",
+  },
+  listItemDate: {
+    color: "#fff",
+    marginRight: 10,
+    color: "#F5DEB3",
+  },
+  listItemTime: {
+    color: "#fff",
+    marginRight: 10,
+    color: "#F5DEB3",
+  },
+  listItemDots: {
+    color: "#fff",
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    borderTopWidth: 1,
+    borderTopColor: "#ccc",
+    paddingVertical: 10,
+  },
+  iconButton: {
+    alignItems: "center",
+  },
+  iconLabel: {
+    fontSize: 12,
+    color: "#888",
+    marginTop: 5,
+  },
+});
+
+export default HomeScreen;
